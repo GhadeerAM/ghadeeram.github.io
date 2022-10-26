e75496aa0af48d8436f57fb8ccd87b1217894f74
GhadeerAM <68711583+GhadeerAM@users.noreply.github.com>
2022-10-26 13:03:11 +0300

    Add files via upload

---
+++ LogFile/LogFile/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LogFile
+{
+    public static class Logger
+    {
+        public static void WriteLog(string status, string root, string app, string creator, string msg)
+        {
+            string filename = "../../Logs/LogFile.txt";
+            //string LogPath = "~/LogFile/Logs";
+            StreamWriter sw = new StreamWriter(filename, append: true);
+            sw.WriteLine("[" + DateTime.Now + "]    (" + status + ")    " + root + "    " + app + "    " + creator + "    :" + msg);
+            sw.Close();
+        }
+    }
+}
---
+++ LogFile/LogFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace LogFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Logger.WriteLog("complete1",
+                "RootMethod1",
+                "MyApp1",
+                "someone1",
+                "oihuhsuccesrdrdsfully1"
+                );
+        }
+       
+      
+    }
+}
