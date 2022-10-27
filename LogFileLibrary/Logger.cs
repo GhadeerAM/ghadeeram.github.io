13eb8cd28b9968590c5959fe7582a833a87727b2
GhadeerAM <68711583+GhadeerAM@users.noreply.github.com>
2022-10-27 11:33:19 +0300

    Add files via upload
    This is the Library of LogFile with the class Logger only.

---
+++ LogFileLibrary/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LogFileLibrary
+{
+   public static class Logger
+    {
+        public static void WriteLog(string pathName,string status, string root, string app, string creator, string msg)
+        {
+            string filename = pathName;
+            StreamWriter sw = new StreamWriter(filename, append: true);
+            sw.WriteLine("[" + DateTime.Now + "]    (" + status + ")    " + root + "    " + app + "    " + creator + "    :" + msg);
+            sw.Close();
+        }
+    }
+}
